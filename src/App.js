4239ac84b44ebd5a0fd72ab4f3de0d336bba6f2e
Simen Kristoffersen <lissomliksom@gmail.com>
2021-12-28 22:36:57 +0100

    Minor updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Gallery from './components/Gallery'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-200 px-2 md:px-8 py-4 md:py-12 min-h-screen">
+      <Header />
+      <Gallery />
     </div>
   );
+
 }
 
 export default App;
