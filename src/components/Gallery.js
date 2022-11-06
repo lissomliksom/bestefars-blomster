580ea0b348399f6e9318c6ffcf7b4fcd6261c690
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-11-06 18:38:35 +0100

    Lag array fra bilder

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,319 +1,34 @@
 import Image from "./Image";
 
-// Import compressed images
-import ImageCompressed1 from "../images/compressed/1_compressed.jpg";
-import ImageCompressed2 from "../images/compressed/2_compressed.jpg";
-import ImageCompressed3 from "../images/compressed/3_compressed.jpg";
-import ImageCompressed4 from "../images/compressed/4_compressed.jpg";
-import ImageCompressed5 from "../images/compressed/5_compressed.jpg";
-import ImageCompressed6 from "../images/compressed/6_compressed.jpg";
-import ImageCompressed7 from "../images/compressed/7_compressed.jpg";
-import ImageCompressed8 from "../images/compressed/8_compressed.jpg";
-import ImageCompressed9 from "../images/compressed/9_compressed.jpg";
-import ImageCompressed10 from "../images/compressed/10_compressed.jpg";
-import ImageCompressed11 from "../images/compressed/11_compressed.jpg";
-import ImageCompressed12 from "../images/compressed/12_compressed.jpg";
-import ImageCompressed13 from "../images/compressed/13_compressed.jpg";
-import ImageCompressed14 from "../images/compressed/14_compressed.jpg";
-import ImageCompressed15 from "../images/compressed/15_compressed.jpg";
-import ImageCompressed16 from "../images/compressed/16_compressed.jpg";
-import ImageCompressed17 from "../images/compressed/17_compressed.jpg";
-import ImageCompressed18 from "../images/compressed/18_compressed.jpg";
-import ImageCompressed19 from "../images/compressed/19_compressed.jpg";
-import ImageCompressed20 from "../images/compressed/20_compressed.jpg";
-import ImageCompressed21 from "../images/compressed/21_compressed.jpg";
-import ImageCompressed22 from "../images/compressed/22_compressed.jpg";
-import ImageCompressed23 from "../images/compressed/23_compressed.jpg";
-import ImageCompressed24 from "../images/compressed/24_compressed.jpg";
-import ImageCompressed25 from "../images/compressed/25_compressed.jpg";
-import ImageCompressed26 from "../images/compressed/26_compressed.jpg";
-import ImageCompressed27 from "../images/compressed/27_compressed.jpg";
-import ImageCompressed28 from "../images/compressed/28_compressed.jpg";
-import ImageCompressed29 from "../images/compressed/29_compressed.jpg";
-import ImageCompressed30 from "../images/compressed/30_compressed.jpg";
-import ImageCompressed31 from "../images/compressed/31_compressed.jpg";
-import ImageCompressed32 from "../images/compressed/32_compressed.jpg";
-import ImageCompressed33 from "../images/compressed/33_compressed.jpg";
-import ImageCompressed34 from "../images/compressed/34_compressed.jpg";
-import ImageCompressed35 from "../images/compressed/35_compressed.jpg";
-import ImageCompressed36 from "../images/compressed/36_compressed.jpg";
-import ImageCompressed37 from "../images/compressed/37_compressed.jpg";
+import { IMAGES } from "../constants/IMAGES";
 
-// Import full images
-import Image1 from "../images/full/1.jpg";
-import Image2 from "../images/full/2.jpg";
-import Image3 from "../images/full/3.jpg";
-import Image4 from "../images/full/4.jpg";
-import Image5 from "../images/full/5.jpg";
-import Image6 from "../images/full/6.jpg";
-import Image7 from "../images/full/7.jpg";
-import Image8 from "../images/full/8.jpg";
-import Image9 from "../images/full/9.jpg";
-import Image10 from "../images/full/10.jpg";
-import Image11 from "../images/full/11.jpg";
-import Image12 from "../images/full/12.jpg";
-import Image13 from "../images/full/13.jpg";
-import Image14 from "../images/full/14.jpg";
-import Image15 from "../images/full/15.jpg";
-import Image16 from "../images/full/16.jpg";
-import Image17 from "../images/full/17.jpg";
-import Image18 from "../images/full/18.jpg";
-import Image19 from "../images/full/19.jpg";
-import Image20 from "../images/full/20.jpg";
-import Image21 from "../images/full/21.jpg";
-import Image22 from "../images/full/22.jpg";
-import Image23 from "../images/full/23.jpg";
-import Image24 from "../images/full/24.jpg";
-import Image25 from "../images/full/25.jpg";
-import Image26 from "../images/full/26.jpg";
-import Image27 from "../images/full/27.jpg";
-import Image28 from "../images/full/28.jpg";
-import Image29 from "../images/full/29.jpg";
-import Image30 from "../images/full/30.jpg";
-import Image31 from "../images/full/31.jpg";
-import Image32 from "../images/full/32.jpg";
-import Image33 from "../images/full/33.jpg";
-import Image34 from "../images/full/34.jpg";
-import Image35 from "../images/full/35.jpg";
-import Image36 from "../images/full/36.jpg";
-import Image37 from "../images/full/37.jpg";
+function importImages(directory) {
+  let images = [];
+  directory.keys().map((item) => {
+    images.push(directory(item));
+  });
+
+  return images;
+}
 
 const Gallery = () => {
-  const images = [
-    {
-      title: "Blomst 1",
-      latin: "latinsk navn kommer",
-      image: Image1,
-      imageCompressed: ImageCompressed1,
-    },
-    {
-      title: "Blomst 2",
-      latin: "latinsk navn kommer",
-      image: Image2,
-      imageCompressed: ImageCompressed2,
-    },
-    {
-      title: "Blomst 3",
-      latin: "latinsk navn kommer",
-      image: Image3,
-      imageCompressed: ImageCompressed3,
-    },
-    {
-      title: "Blomst 4",
-      latin: "latinsk navn kommer",
-      image: Image4,
-      imageCompressed: ImageCompressed4,
-    },
-    {
-      title: "Blomst 5",
-      latin: "latinsk navn kommer",
-      image: Image5,
-      imageCompressed: ImageCompressed5,
-    },
-    {
-      title: "Blomst 6",
-      latin: "latinsk navn kommer",
-      image: Image6,
-      imageCompressed: ImageCompressed6,
-    },
-    {
-      title: "Blomst 7",
-      latin: "latinsk navn kommer",
-      image: Image7,
-      imageCompressed: ImageCompressed7,
-    },
-    {
-      title: "Blomst 8",
-      latin: "latinsk navn kommer",
-      image: Image8,
-      imageCompressed: ImageCompressed8,
-    },
-    {
-      title: "Blomst 9",
-      latin: "latinsk navn kommer",
-      image: Image9,
-      imageCompressed: ImageCompressed9,
-    },
-    {
-      title: "Blomst 10",
-      latin: "latinsk navn kommer",
-      image: Image10,
-      imageCompressed: ImageCompressed10,
-    },
-    {
-      title: "Blomst 11",
-      latin: "latinsk navn kommer",
-      image: Image11,
-      imageCompressed: ImageCompressed11,
-    },
-    {
-      title: "Blomst 12",
-      latin: "latinsk navn kommer",
-      image: Image12,
-      imageCompressed: ImageCompressed12,
-    },
-    {
-      title: "Blomst 13",
-      latin: "latinsk navn kommer",
-      image: Image13,
-      imageCompressed: ImageCompressed13,
-    },
-    {
-      title: "Blomst 14",
-      latin: "latinsk navn kommer",
-      image: Image14,
-      imageCompressed: ImageCompressed14,
-    },
-    {
-      title: "Blomst 15",
-      latin: "latinsk navn kommer",
-      image: Image15,
-      imageCompressed: ImageCompressed15,
-    },
-    {
-      title: "Blomst 16",
-      latin: "latinsk navn kommer",
-      image: Image16,
-      imageCompressed: ImageCompressed16,
-    },
-    {
-      title: "Blomst 17",
-      latin: "latinsk navn kommer",
-      image: Image17,
-      imageCompressed: ImageCompressed17,
-    },
-    {
-      title: "Blomst 18",
-      latin: "latinsk navn kommer",
-      image: Image18,
-      imageCompressed: ImageCompressed18,
-    },
-    {
-      title: "Blomst 19",
-      latin: "latinsk navn kommer",
-      image: Image19,
-      imageCompressed: ImageCompressed19,
-    },
-    {
-      title: "Blomst 20",
-      latin: "latinsk navn kommer",
-      image: Image20,
-      imageCompressed: ImageCompressed20,
-    },
-    {
-      title: "Blomst 21",
-      latin: "latinsk navn kommer",
-      image: Image21,
-      imageCompressed: ImageCompressed21,
-    },
-    {
-      title: "Blomst 22",
-      latin: "latinsk navn kommer",
-      image: Image22,
-      imageCompressed: ImageCompressed22,
-    },
-    {
-      title: "Blomst 23",
-      latin: "latinsk navn kommer",
-      image: Image23,
-      imageCompressed: ImageCompressed23,
-    },
-    {
-      title: "Blomst 24",
-      latin: "latinsk navn kommer",
-      image: Image24,
-      imageCompressed: ImageCompressed24,
-    },
-    {
-      title: "Blomst 25",
-      latin: "latinsk navn kommer",
-      image: Image25,
-      imageCompressed: ImageCompressed25,
-    },
-    {
-      title: "Blomst 26",
-      latin: "latinsk navn kommer",
-      image: Image26,
-      imageCompressed: ImageCompressed26,
-    },
-    {
-      title: "Blomst 27",
-      latin: "latinsk navn kommer",
-      image: Image27,
-      imageCompressed: ImageCompressed27,
-    },
-    {
-      title: "Blomst 28",
-      latin: "latinsk navn kommer",
-      image: Image28,
-      imageCompressed: ImageCompressed28,
-    },
-    {
-      title: "Blomst 29",
-      latin: "latinsk navn kommer",
-      image: Image29,
-      imageCompressed: ImageCompressed29,
-    },
-    {
-      title: "Blomst 30",
-      latin: "latinsk navn kommer",
-      image: Image30,
-      imageCompressed: ImageCompressed30,
-    },
-    {
-      title: "Blomst 31",
-      latin: "latinsk navn kommer",
-      image: Image31,
-      imageCompressed: ImageCompressed31,
-    },
-    {
-      title: "Blomst 32",
-      latin: "latinsk navn kommer",
-      image: Image32,
-      imageCompressed: ImageCompressed32,
-    },
-    {
-      title: "Blomst 33",
-      latin: "latinsk navn kommer",
-      image: Image33,
-      imageCompressed: ImageCompressed33,
-    },
-    {
-      title: "Blomst 34",
-      latin: "latinsk navn kommer",
-      image: Image34,
-      imageCompressed: ImageCompressed34,
-    },
-    {
-      title: "Blomst 35",
-      latin: "latinsk navn kommer",
-      image: Image35,
-      imageCompressed: ImageCompressed35,
-    },
-    {
-      title: "Blomst 36",
-      latin: "latinsk navn kommer",
-      image: Image36,
-      imageCompressed: ImageCompressed36,
-    },
-    {
-      title: "Blomst 37",
-      latin: "latinsk navn kommer",
-      image: Image37,
-      imageCompressed: ImageCompressed37,
-    },
-  ];
+  const bilderFull = importImages(
+    require.context("../images/full", false, /\.(png|jpe?g|svg)$/)
+  );
+  const bilderCompressed = importImages(
+    require.context("../images/compressed", false, /\.(png|jpe?g|svg)$/)
+  );
 
   return (
     <main>
       <div className="container-xl p-4 grid grid-cols-2 md:grid-cols-3 gap-2 mx-auto">
-        {images.map((image, idx) => (
+        {bilderCompressed.map((bilde, idx) => (
           <Image
             key={idx}
-            title={image.title}
-            latin={image.latin}
-            source={image.image}
-            sourceCompressed={image.imageCompressed}
+            title={IMAGES[idx].title}
+            latin={IMAGES[idx].latin}
+            source={bilderFull[idx]}
+            sourceCompressed={bilde}
           />
         ))}
       </div>
